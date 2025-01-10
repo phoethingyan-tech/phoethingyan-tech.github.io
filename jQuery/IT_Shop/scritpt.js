a234814791c7bbfcc8b93b1b4f6155a34b8898d1
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-01-10 10:55:05 +0630

    update lesson

--- jQuery/IT_Shop/scritpt.js
+++ jQuery/IT_Shop/scritpt.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     count();
+    getData();
     $('.addToCart').click(function(){
         let id = $(this).data('id');
         let name = $(this).data('name');
@@ -62,5 +63,99 @@ $(document).ready(function() {
             });
         }
     }
+
+    function getData() {
+        let itemString = localStorage.getItem('shops');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+
+            let data = '';
+            let no = 1;
+            let total = 0;
+            $.each(itemArray,function(i,v) {
+                let name = v.name;
+                let price = v.price;
+                let qty = v.qty;
+
+                data += `<tr>
+                    <td>${no++}</td>
+                    <td>${name}</td>
+                    <td>${price}</td>
+                    <td>
+                    <button class="min" data-key="${i}">-</button>
+                        ${qty}
+                    <button class="max" data-key="${i}">+</button>
+                    </td>
+                    <td>${price * qty}</td>
+                </tr>`;
+                total += price*qty;
+            });
+
+            data += `<tr>
+                <td colspan="4" align="right">Total</td>
+                <td>${total}</td>
+            </tr>`
+            $('#item-tbody').html(data);
+        }
+    };
+
+    $('#item-tbody').on('click','.min',function() {
+        let key = $(this).data('key');
+        let itemString = localStorage.getItem('shops');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+
+            $.each(itemArray, function(i,v){
+                if(key == i) {
+                    v.qty--;
+                    if(v.qty == 0){
+                        let ans = confirm('Are you sure!');
+                        if(ans) {
+                            itemArray.splice(key,1);
+                        }
+                        else {
+                            v.qty = 1;
+                        }                        
+                    }
+                }
+            });
+
+            let itemData = JSON.stringify(itemArray);
+            localStorage.setItem('shops',itemData);
+
+            getData();
+            count();
+        }
+    });
+
+    $('#item-tbody').on('click','.max',function() {
+        let key = $(this).data('key');
+        let itemString = localStorage.getItem('shops');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+
+            $.each(itemArray, function(i,v){
+                if(key == i) {
+                    v.qty++;                                        
+                }
+
+                
+            });
+
+            let itemData = JSON.stringify(itemArray);
+            localStorage.setItem('shops',itemData);
+
+            getData();
+            count();
+        }
+    });
+
+    $('#order').click(function() {
+        let ans = confirm('Are you sure Order Now');
+        if(ans) {
+            localStorage.clear('shops');
+            window.location.href='index.html';
+        }
+    });
 
 });
